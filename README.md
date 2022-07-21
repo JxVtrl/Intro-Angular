f82f744011481ab5aac614e9790c39b29c3cbda1
Joao <joaoviniciusvitral@hotmail.com>
2022-07-21 01:10:13 -0300

    new studies

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Para criar o projeto em Angular, utilize o comando:
 ```
 ng new <projeto-angular>
 ```
+
+Para abrir o projeto em Angular, utilize o comando:
+```
+ng serve -o
+```
--- teste/src/app/app.component.ts
+++ teste/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // onde o componente será renderizado
+  templateUrl: './app.component.html', // parte visual do componente
+  // styleUrls: ['./app.component.css'] // parte de estilização do componente
+
+  // template: `
+  //   <h1>Meu template sem criar .html</h1>
+  // `
 })
 export class AppComponent {
   title = 'teste';
+  nomes = ['João', 'Maria', 'José'];
+  meuNome = this.nomes[0];
 }
